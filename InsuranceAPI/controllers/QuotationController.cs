85adc49a2fad2a6d794f1c67c00da646867b509b
Emmanuel BLONVIA <reallysmartguys@gmail.com>
2024-12-05 10:34:33 +0000

    initilisation du projet

---
+++ InsuranceAPI/controllers/QuotationController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsuranceAPI.controllers;
+[ApiController]
+[Route("/quotes")]
+public class QuotationController
+{
+    
+    
+    //TODO faire la méthode d'entrée
+    // TODO avec les paramètres nécessaire
+    //TODO Le controller doit appeler le UseCase : GetQuotation
+    //TODO La classe GetQuotation doit utiliser QuotationService pour réaliser le travail
+    //TODO Enregistrer la quotation en base de donnnée. 
+    //TODO : @Emmanuel BLONVIA
+}
